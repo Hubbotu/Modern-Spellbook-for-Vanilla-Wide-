20ae92d3ff3c4f6a2a820323ee14ab9c7bc2854c
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-11-17 07:08:14 +0300

    Add files via upload

---
+++ Localization.lua
@@ -0,0 +1,38 @@
+ModernSpellBookFrame.Locales = {}
+
+-- Supported locales are:
+-- English (enUS)
+-- French (frFR)
+-- German (deDE)
+-- Spanish (esES)
+-- Russian (ruRU) ZamestoTV
+
+ModernSpellBookFrame.Locales["enUS"] = {}
+local localeDict = ModernSpellBookFrame.Locales["enUS"]
+localeDict["ShowPassive"] = "Show passives"
+localeDict["SearchAbilities"] = "Search abilities, keywords"
+localeDict["NoResults"] = "No results. Try a different search term.|nFor example, '"
+
+ModernSpellBookFrame.Locales["frFR"] = {}
+local localeDict = ModernSpellBookFrame.Locales["frFR"]
+localeDict["ShowPassive"] = "Afficher les passifs"
+localeDict["SearchAbilities"] = "Capacités de recherche, mots-clés"
+localeDict["NoResults"] = "Aucun résultat. Essayez un autre terme de recherche.|nPar exemple, '"
+
+ModernSpellBookFrame.Locales["deDE"] = {}
+local localeDict = ModernSpellBookFrame.Locales["deDE"]
+localeDict["ShowPassive"] = "Passive anzeigen"
+localeDict["SearchAbilities"] = "Suchfunktionen, Schlüsselwörter"
+localeDict["NoResults"] = "Keine Ergebnisse. Versuchen Sie es mit einem anderen Suchbegriff.|nZum Beispiel '"
+
+ModernSpellBookFrame.Locales["esES"] = {}
+local localeDict = ModernSpellBookFrame.Locales["esES"]
+localeDict["ShowPassive"] = "Mostrar pasivos"
+localeDict["SearchAbilities"] = "Capacidades de búsqueda, palabras clave"
+localeDict["NoResults"] = "Sin resultados. Pruebe con un término de búsqueda diferente.|nPor ejemplo '"
+
+ModernSpellBookFrame.Locales["ruRU"] = {}
+local localeDict = ModernSpellBookFrame.Locales["ruRU"]
+localeDict["ShowPassive"] = "Показать пассивные"
+localeDict["SearchAbilities"] = "Поиск способностей, ключевые слова"
+localeDict["NoResults"] = "Нет результатов. Попробуйте другой поисковый запрос.|nНапример, '"
